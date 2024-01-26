b48c8eb819d17a3c79f17fd92f65cb39b461a40f
C A <chadandari@outlook.com>
2024-01-26 18:59:34 -0500

    Category routes finished

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,8 +45,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const catPut = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!catPut[0]) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(catPut);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
