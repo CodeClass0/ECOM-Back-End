d3c9d097151095a771d93d190367d880a1794941
C A <chadandari@outlook.com>
2024-01-26 18:57:09 -0500

    category delete works

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,8 +49,25 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+
+  try {
+    const catDelete = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!catDelete) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(catDelete);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
